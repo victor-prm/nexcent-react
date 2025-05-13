1bb5ed5692faa861386fb133362d6c07189a11a4
victor-prm <victorpabb@gmail.com>
2025-05-13 10:47:01 +0200

    did some styling

--- src/components/MarketFit/MarketFit.jsx
+++ src/components/MarketFit/MarketFit.jsx
@@ -6,29 +6,31 @@ import MarketFitCard from './MarketFitCard'
 
 function MarketFit() {
     let cards = [
-        {   
+        {
             icon: icon1,
             title: "Membership Organisations",
             text: "Our membership management software provides full automation of membership renewals and payments"
         },
-        {   
+        {
             icon: icon2,
             title: "National Associations",
             text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, quas!"
         },
-        {   
+        {
             icon: icon3,
             title: "Clubs And Groups",
             text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit excepturi vitae cum!"
         },
     ]
 
-    
+
 
     return (
         <section className='market-fit'>
-            <h2>Manage your entire community in a single system</h2>
-            <p>Who is Nextcent suitable for?</p>
+            <hgroup className='section-headline'>
+                <h2>Manage your entire community in a single system</h2>
+                <p>Who is Nextcent suitable for?</p>
+            </hgroup>
             <ul className='market-fit__list'>
                 {cards.map(card => (<MarketFitCard data={card} />))}
             </ul>
